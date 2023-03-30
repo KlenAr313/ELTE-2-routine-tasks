c084b0b09f89291e499412ddca0c89c3813a087e
Klenovszky Áron <h9mrbv@INF.ELTE.HU>
2023-03-30 11:21:52 +0200

    Invoice

---
+++ OOP/Invoice_skeleton/Invoice/InFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace Invoice
+{
+    public class InFile
+    {
+        private readonly TextFileReader reader;
+
+        public InFile(string inputPath)
+        {
+            reader = new TextFileReader(inputPath);
+        }
+
+        public Invoice? Read()
+        {
+            Invoice? invoice = null;
+
+            if (reader.ReadLine(out string line))
+            {
+                char[] separators = { ' ', '\t' };
+                string[] items = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                invoice = new Invoice(items[0]);
+                for (int i = 1; i < items.Length; i += 4)
+                {
+                    Product c = new Product
+                    {
+                        name = items[i],
+                        price = int.Parse(items[i + 1])
+                    };
+
+                    invoice.Add(c);
+                }
+            }
+
+            return invoice;
+        }
+    }
+}
---
+++ OOP/Invoice_skeleton/Invoice/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Invoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InFile file = new("input.txt");
+                Invoice? invoice;
+                int income = 0;
+
+                while ((invoice = file.Read()) != null)
+                {
+                    income += invoice.Sum;
+                }
+
+                Console.WriteLine($"Teljes bevetel: {income}");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("A megadott fajl nem letezik!");
+            }
+        }
+    }
+}
