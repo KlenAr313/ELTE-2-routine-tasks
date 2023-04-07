1e0774135587c45a8a68acfa7be6653a35a32dd3
Klenovszky Áron <aron.klenovszky@gmail.com>
2023-04-07 22:13:15 +0200

    Suffering

---
+++ OOP/Temperature/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            double average = 0;
+            bool l = false;
+            double min = 0;
+            double e = 0;
+            double s = 0;
+            int db = 0;
+            try
+            {
+                StreamReader x = new StreamReader("inp.txt");
+                List<double> list = new List<double>();
+                while (!x.EndOfStream)
+                {
+                    string? line = x.ReadLine();
+                    if (line != null)
+                    {
+                        string[] data = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int j = 0; j < data.Length; j++)
+                        {
+                            if (double.TryParse(data[j], out e))
+                            {
+                                list.Add(e);
+                            }
+                        }
+
+                    }
+                }
+                int i = 0;
+                for (; i < list.Count && list[i] >= 0; i++)
+                {
+                    s += list[i];
+                    db++;
+                }
+
+                average = s / db;
+                l = true;
+                min = list[i];
+
+                for (; i < list.Count; i++)
+                {
+                    l = l && list[i] < 0;
+                    min = Math.Min(min, list[i]);
+                }
+
+                x.Close();
+                x.Dispose();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Nem létezik a megadott input fájl!");
+            }
+
+            Console.WriteLine($"Atlag: {Math.Round(average, 2)}\nMindig fagyott: {l}\nLegalacsonyabb: {min}");
+        }
+    }
+}
