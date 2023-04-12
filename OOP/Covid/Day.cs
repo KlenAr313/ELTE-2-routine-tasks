1b5e896be7d7f2af9f70d45cb2d49e00005286a8
Klenovszky Áron <aron.klenovszky@gmail.com>
2023-04-12 09:30:44 +0200

    Cactus

---
+++ OOP/Covid/Day.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid
+{
+    internal class Day
+    {
+        public string date { get; set; }
+        public int fresh { get; set; }
+        public int sum { get; set; }
+
+        public Day(string date, int full, int sum)
+        {
+            this.date = date; 
+            this.fresh = full;
+            this.sum = sum;
+        }
+    }
+}
---
+++ OOP/Covid/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Covid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InFile reader = new InFile("inp.txt");
+                bool I = false;
+                int max = 0;
+                string maxDate = "";
+                while (!reader.end())
+                {
+                    Day? day = reader.read();
+                    if (day != null )
+                    {
+                        I = I || day.fresh > 5000;
+                        if(max < day.sum)
+                        {
+                            max = day.sum;
+                            maxDate = day.date;
+                        }
+                    }
+                }
+                reader.Dispose();
+                Console.WriteLine((I ? "yes" : "no") + " " + maxDate);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Nem található ilyen file!");
+            }
+        }
+    }
+}
